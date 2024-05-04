a8594b77b87da738fa453b5f0296e89258247a0a
Shanu sarkar <sanumts484@gmail.com>
2024-05-04 20:25:44 +0530

    shanu

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["origin: [process.env.FRONTEND_URL"],
+    origin: [process.env.FRONTEND_URL],,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
