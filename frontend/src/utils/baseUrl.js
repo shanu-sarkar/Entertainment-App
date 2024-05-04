58a0af2d2512f410e98bb6067e33d04aa2454a3b
Shanu sarkar <sanumts484@gmail.com>
2024-05-04 21:39:39 +0530

    sssss

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 // const baseUrl = "http://localhost:8000/api";
-const baseUrl = "https://entertainment-app-backendshanu.vercel.app/";
+const baseUrl = "https://entertainment-app-backendshanu.vercel.app/api";
 
 export default baseUrl;
