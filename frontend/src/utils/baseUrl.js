8fdd8f557de54dc22707d0c5ab356f1aad93b327
Shanu sarkar <sanumts484@gmail.com>
2024-05-04 20:05:23 +0530

    api Change

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "http://localhost:8000/api";
+// const baseUrl = "http://localhost:8000/api";
+const baseUrl = "https://entertainment-app-backendshanu.vercel.app/";
 
 export default baseUrl;
